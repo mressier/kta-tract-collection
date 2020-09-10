e28a1e330df8444f1b455a1a4a4c95a0b84edb3f
Mathilde Ressier <mathilderessier@hotmail.fr>
2020-09-10 21:35:51 +0200

    Add fields to tract model

--- KtaTractCollection/app/src/main/java/com/onion/ktatractcollection/Models/Tract.kt
+++ KtaTractCollection/app/src/main/java/com/onion/ktatractcollection/Models/Tract.kt
@@ -4,13 +4,32 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
 
+/**
+ * A class representing a [Tract]: this is a piece of paper or an object found in the catacombs,
+ * usually concealed by a cataphile between two stones or somewhere else.
+ */
 @Entity
 data class Tract(
     @PrimaryKey val id: UUID = UUID.randomUUID(),
+    /* Date of the creation of the tract in the database */
+    val databaseAddingDate: Date = Date(),
+    /* Author of the tract */
     var author: String = "",
+    /* Date when tract has been discovered */
     var discoveryDate: Date = Date(),
+    /* Date figuring on the tract */
+    var tractDateYear: Int? = null,
+    var tractDateMonth: Int? = null,
+    var tractDateDay: Int? = null,
+    /* Comments about the tract */
     var comment: String = "") {
 
-    val photoFilename
-        get() = "IMG_$id.jpg"
+    val tractDate: Date?
+        get() {
+            return tractDateYear?.let { year ->
+                val calendar = Calendar.getInstance()
+                calendar.set(year, tractDateMonth ?: 1, tractDateDay ?: 1)
+                calendar.time
+            }
+        }
 }
